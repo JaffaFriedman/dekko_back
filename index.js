c18c68ecf14950cef4ebe6a7c8b2666e665125ee
JaffaFriedman <jafriedman.dwfsc4@bootcampudd.cl>
2023-07-31 12:49:12 -0400

    Modificaciones clase

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ app.use(userRouter);
 app.use(productRouter);
 app.use(orderRouter);
 app.listen(process.env.PORT, () => console.log(`Servidor conectado en puerto: ${process.env.PORT}`))
+
+/* Clase 
+
+app.get('/',(req,res)=> { res.status(200).json{mensaje:'ruta get'}})
+
+app.listen(4000, ()=>{console.log('escuchando en el puerto 4000')})
+
+app.put('/',(req,res)=> { res.status(200).json{mensaje:'ruta put'}})
+*/
